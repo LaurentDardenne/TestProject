bd857a6a0cd364dd1470c6520e6deb2ae5195e77
LaurentDardenne <LaDardenne@Orange.fr>
2016-09-15 22:57:40 +0200

    Mise au point du tempalte Clone Suppression des dépendances du profile PS local

--- PostAction.ps1
+++ PostAction.ps1
@@ -1,27 +1,34 @@
 ﻿#todo proxy de Invoke-Plaster?
-
+  param(
+    #todo exclusif
+    [switch] $Create,
+    [switch] $Clone
+  )
 $PSGalleryPublishUri = 'https://www.myget.org/F/ottomatt/api/v2/package'
 $PSGallerySourceUri = 'https://www.myget.org/F/ottomatt/api/v2'
-try{
- Get-PSRepository OttoMatt -EA Stop >$null   
-}catch {
-  if ($_.CategoryInfo.Category -ne 'ObjectNotFound')
-  { throw $_ }
-  else
-  { Register-PSRepository -Name OttoMatt -SourceLocation $PSGallerySourceUri -PublishLocation $PSGalleryPublishUri -InstallationPolicy Trusted }
-}
-
- #On installe les prérequis sur le poste pas dans le référentiel projet
- # La tâche de build doit pointer sur ces scripts 
- #user : "${env:USERPROFILE}\Documents\WindowsPowerShell\Scripts"
- #all : "${env:ProgramFiles}\WindowsPowerShell\Scripts
-'Replace-String','Lock-File','Remove-Conditionnal',
-'Show-BalloonTip','Test-BOMFile','Using-Culture'|
-  Foreach {
-   Install-Script -name "$_" -Repository OttoMatt -Scope AllUsers 
+function Set-CommonDependency{
+  param()
+  try{
+  Get-PSRepository OttoMatt -EA Stop >$null   
+  }catch {
+    if ($_.CategoryInfo.Category -ne 'ObjectNotFound')
+    { throw $_ }
+    else
+    { Register-PSRepository -Name OttoMatt -SourceLocation $PSGallerySourceUri -PublishLocation $PSGalleryPublishUri -InstallationPolicy Trusted }
   }
-"Log4Posh","DTW.PS.FileSystem" |
- Foreach {
-   Install-Module -name $_ -Repository OttoMatt -Scope AllUsers
- }
- 
+
+  #On installe les prérequis sur le poste pas dans le référentiel projet
+  # La tâche de build doit pointer sur ces scripts 
+  #user : "${env:USERPROFILE}\Documents\WindowsPowerShell\Scripts"
+  #all : "${env:ProgramFiles}\WindowsPowerShell\Scripts
+  'Replace-String','Lock-File','Remove-Conditionnal',
+  'Show-BalloonTip','Test-BOMFile','Using-Culture'|
+    Foreach {
+    Install-Script -name "$_" -Repository OttoMatt -Scope AllUsers 
+    }
+  "Log4Posh","DTW.PS.FileSystem" |
+  Foreach {
+    Install-Module -name $_ -Repository OttoMatt -Scope AllUsers
+  }
+}
+Set-CommonDependency
--- Templates/PlasterClone/Templates/PostInstall.T.ps1
+++ Templates/PlasterClone/Templates/PostInstall.T.ps1
@@ -13,6 +13,7 @@ $Delivery,$logs|
 Copy-item '<!%=${PLASTER_DestinationPath}%>\<%=${PLASTER_PARAM_ProjectName}%>.library-ms' '<!%=${Env:AppData}%>\Microsoft\Windows\Libraries\<%=${PLASTER_PARAM_ProjectName}%>.library-ms' -Force
 
 #Project profile 
-if (!(Test-Path "$PSProfile\ProjectsProfile"))
- ( New-Item "$PSProfile\ProjectsProfile" -ItemType Directory > $null}   
-Copy-item '<!%=${PLASTER_DestinationPath}%>\<%=${PLASTER_PARAM_ProjectName}%>_ProjectProfile.ps1' "$PSProfile\ProjectsProfile" -Force
+$UserPSProfile=split-path $profile.CurrentUserAllHosts -Parent
+if (!(Test-Path "$UserPSProfile\ProjectsProfile"))
+ ( New-Item "$UserPSProfile\ProjectsProfile" -ItemType Directory > $null}   
+Copy-item '<!%=${PLASTER_DestinationPath}%>\<%=${PLASTER_PARAM_ProjectName}%>_ProjectProfile.ps1' "$UserPSProfile\ProjectsProfile" -Force
